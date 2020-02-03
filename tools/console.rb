d155697b0d24b4131b46f676325cedf75d9016c8
github username <github email address>
2020-02-03 12:17:53 -0500

    updates

--- tools/console.rb
+++ tools/console.rb
@@ -12,10 +12,10 @@ cosmo = Magazine.new("Cosmo", 5)
 essence = Magazine.new("Essence", 3)
 people = Magazine.new("People", 4)
 
-sub1 = Subscription(sonata, cosmo, Time.now)
-sub2 = Subscription(lauren, people, Time.new(2018, 1, 1))
-sub3 = Subscription(yo, people, Time.new(2019, 11, 12))
-sub4 = Subscription(sonata, essence, Time.new(2017, 10, 8))
+sub1 = Subscription.new(sonata, cosmo, Time.now)
+sub2 = Subscription.new(lauren, people, Time.new(2018, 1, 1))
+sub3 = Subscription.new(yo, people, Time.new(2019, 11, 12))
+sub4 = Subscription.new(sonata, essence, Time.new(2017, 10, 8))
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
